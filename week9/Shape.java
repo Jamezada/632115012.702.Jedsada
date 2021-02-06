0df48d89acddc66b7ac10ebce8bbc09e5faadc6e
Jedsada Kampen <77324151+Jamezada@users.noreply.github.com>
2021-02-06 17:08:59 +0700

    week9

--- week9/Shape.java
+++ week9/Shape.java
@@ -1,26 +1,44 @@
 package week9;
 
 public class Shape {
-    public String toString(){
+    public String toString() {
         return "This is a shape";
+    }
+
+    public static void main(String[] args) {
+        Cube cube1 = new Cube(5, 5, 5);
+
+        System.out.println(cube1.toString());
     }
 }
 
 class Rectangle extends Shape {
-    int width;
-    int height;
-    
-    public Rectangle(int width, int height){
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
         this.width = width;
         this.height = height;
+    }
+
+    // overiding toString()
+    @Override
+    public String toString() {
+        return super.toString() + "\nThis is a rectangle with width as " + width + " and height as " + height;
     }
 }
 
 class Cube extends Rectangle {
-    int length;
+    private int length;
 
-    public Cube(int width, int height, int length){
+    public Cube(int width, int height, int length) {
         super(width, height);
         this.length = length;
+    }
+
+    // overiding toString()
+    @Override
+    public String toString() {
+        return super.toString() + "\nThis is a cube with the side of " + length;
     }
 }
