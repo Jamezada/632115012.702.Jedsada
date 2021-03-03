77291fabca3e8decb4ecc0403796fad901fecbe9
Jedsada Kampen <77324151+Jamezada@users.noreply.github.com>
2021-03-03 22:01:04 +0700

    week11

---
+++ week11/InstanceofDemo.java
@@ -0,0 +1,30 @@
+package week11;
+
+public class InstanceofDemo {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        Object c = new C();
+
+        //Test the instan of object and class
+        System.out.println(a instanceof A);
+        System.out.println(b instanceof B);
+        System.out.println(b instanceof A);
+        System.out.println(a instanceof B);
+        System.out.println(c instanceof C);
+        System.out.println(c instanceof A);
+        System.out.println(c instanceof B);
+    }
+}
+
+class A {
+
+}
+
+class B extends A {
+
+}
+
+class C {
+
+}
